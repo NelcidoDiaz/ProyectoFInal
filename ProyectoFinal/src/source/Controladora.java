fe34e8df282492ba6da4023a4b26f8cacb9e4a73
gesbi <gesbi@DESKTOP-9P26G06>
2021-07-14 22:38:21 -0400

    Clases Modificadas - Controladora   Se le agrego las funciones de busqueda
    - Factura
    Se agrego codFactura

--- ProyectoFinal/src/source/Controladora.java
+++ ProyectoFinal/src/source/Controladora.java
@@ -16,4 +16,77 @@ public class Controladora {
 		ordenCompra = new ArrayList<OrdenCompra>();
 	}
 	
+	public ArrayList<Componente> getMisComponentes() {
+		return misComponentes;
+	}
+
+	public void setMisComponentes(ArrayList<Componente> misComponentes) {
+		this.misComponentes = misComponentes;
+	}
+
+	public ArrayList<Factura> getMisFacturas() {
+		return misFacturas;
+	}
+
+	public void setMisFacturas(ArrayList<Factura> misFacturas) {
+		this.misFacturas = misFacturas;
+	}
+
+	public Cliente[] getMisClientes() {
+		return misClientes;
+	}
+
+	public void setMisClientes(Cliente[] misClientes) {
+		this.misClientes = misClientes;
+	}
+
+	public ArrayList<OrdenCompra> getOrdenCompra() {
+		return ordenCompra;
+	}
+
+	public void setOrdenCompra(ArrayList<OrdenCompra> ordenCompra) {
+		this.ordenCompra = ordenCompra;
+	}
+
+	public Componente buscComponente(String numSerie) {
+		Componente aux = null;
+		boolean existe = false;
+		int i = 0;
+		while(!existe && i < misComponentes.size()){
+			if(misComponentes.get(i).getNumeroDeSerie().equalsIgnoreCase(numSerie)) {
+				aux = misComponentes.get(i);
+				existe = true;
+			}
+		}
+		return aux;
+	}
+
+	public Factura buscFactura(String codFac) {
+		Factura aux = null;
+		boolean existe  = false;
+		int i = 0;
+		while(!existe && i<misFacturas.size()) {
+			if(misFacturas.get(i).getCodFactura().equalsIgnoreCase(codFac)) {
+				aux = misFacturas.get(i);
+				existe = true;
+			}
+			i++;
+		}
+		return aux;
+	}
+	
+	public Cliente buscCliente(String cedula) {
+		Cliente aux = null;
+		boolean existe = false;
+		int i = 0;
+		while(!existe&& i < misClientes.length) {
+			if(misClientes[i].getCedula().equalsIgnoreCase(cedula)) {
+				aux = misClientes[i];
+				existe = true;
+			}
+			i++;
+		}
+		return aux;
+	}
+	
 }
